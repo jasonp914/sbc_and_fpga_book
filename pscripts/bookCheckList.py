37c699234d41ff452d3c09fcc910cc1cc9c84863
OriginalPI <jason@jasonpenningtonphd.com>
2018-08-29 00:44:55 +0000

    Chapter names of all the chapters and can todo tag init in the python script

--- pscripts/bookCheckList.py
+++ pscripts/bookCheckList.py
@@ -1,15 +1,39 @@
+#!/usr/bin/python
+
 import numpy as py
 import re
 #import pandas as pd
 
 def parse_file(filepath):
 	data = []
+	lc=0
 	with open(filepath,'r') as file_object:
 		line = file_object.readline()
 		while line:
-			print(line)
+			data.append(line)
+			lc=lc+1
 			line = file_object.readline()
+	return data
 		
+def add_todo_init(data_list,filepath):	
+	# File is parsed - print out file
+	file=open(filepath,'w')
 
-print("Hello World")
-parse_file("../texfiles/chapter1.tex")
+
+	for i in range(0,len(data_list)):
+		# Copy line over to new file
+		file.write(data_list[i]+"\n")
+		
+		# Determine if we need to write TODO tag
+		if data_list[i].find("chapter") == 1:
+			file.write("\t<TODO Chapter : NOT DONE>\n")
+			print("Found C")
+		elif data_list[i].find("section") == 1:
+			file.write("\t<TODO Section : NOT DONE>\n")
+			print("Found S")
+
+	file.close()		
+
+fn = "../texfiles/chapter1.tex"
+data = parse_file(fn)
+add_todo_init(data,fn)
